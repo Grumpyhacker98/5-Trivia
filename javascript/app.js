8308feeffcff7642952de1176fe65db1a0ea8501
Max Lewis <max.a.lewis98@gmail.com>
2020-01-08 22:48:54 -0700

    psuedocode

---
+++ javascript/app.js
@@ -0,0 +1,45 @@
+trivia = {
+    trial1: {
+        question: "",
+        answer: true,
+    },
+    trial2: {
+        question: "",
+        answer: true,
+    },
+    trial3: {
+        question: "",
+        answer: true,
+    },
+    trial4: {
+        question: "",
+        answer: true,
+    },
+    trial5: {
+        question: "",
+        answer: true,
+    },
+}
+
+correct = 0;
+wrong = 0;
+
+// to prevent game from working when not capable
+game-logic = false;
+
+// game logic function (the answer value)
+// push a trial
+// true or false logic
+// push correct incorrect values
+
+// reset game function
+
+// timers for questions
+// on buttonclick turn off the game-logic until the timer is off then reverse it
+// 3 seconds for the awnser
+// 7 seconds for the guessing phase
+
+
+// on page load and when the timers allow
+// the 4 answer buttons
+// function(answer-value)
